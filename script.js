264362a8668f73d43006a579f27d5bc457b3f78c
Zylyi <73146812+Zylyi@users.noreply.github.com>
2022-09-18 23:15:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,95 @@
+const form = document.querySelector('.register__form');
+const inner = document.querySelector('.register__inner');
+const registerButton = document.querySelector('.register__reg');
+const loginButton = document.querySelector('.register__login');
+const h1 = document.querySelector('.register__inner h1');
+const h2 = document.querySelector('.info');
+const nameForm = document.querySelector('.register__name');
+const passwordForm = document.querySelector('.register__password');
+
+const user = {
+    name: '',
+    password: '',
+}
+const symbolsList = ['_', '+', '-', '='];
+const arr_ru = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ы', 'ъ', 'э', 'ю', 'я'];
+const arr_RU = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ы', 'Ъ', 'Э', 'Ю', 'Я'];  
+
+let letterBig = '';
+let symbol = '';
+let rus = '';
+
+const anim = setInterval(() => {
+    inner.classList.toggle('animation');
+    inner.style['transition'] = '1s';
+}, 2500);
+    
+function registerForm () {
+    h1.innerText = "Registration";
+    let name = nameForm.value,
+        password = passwordForm.value;
+
+    if (name == user.name && password == user.password) {
+        h2.innerText = `login ${name} используется`;
+        user.name = '';
+    } 
+
+    user.name = name;
+    user.password = password;
+
+    if (password.length < 2) {
+        passwordForm.classList.toggle('wrong-password');
+        h2.innerText = 'password short';
+        user.name = '';
+        user.password = '';
+
+    } else {
+        h2.innerText = '';
+        passwordForm.classList.remove('wrong-password');
+    }
+    
+    const passwordCase = password.split('').map((letter) => {
+        if (letter.toUpperCase() == letter) {
+            letterBig++;
+        }
+        for (let i = 0; i < symbolsList.length; i++) {
+            if (symbolsList[i] == letter) {
+                symbol++;
+            }
+        }
+        for (let i = 0; i < arr_ru.length; i++) {
+            if (arr_RU[i] == letter) {
+                rus++;
+            }
+            if (arr_ru[i] == letter) {
+                rus++;
+            }
+        }
+    });
+    
+
+    if (rus >= 1 || letterBig < 3 || symbol < 2) {
+        h2.innerText = 'password bad';
+        user.name = '';
+        user.password = '';
+        passwordForm.classList.toggle('wrong-password');
+    } else {
+        h1.innerText = "autorisation";
+    }
+
+    form.reset()
+}
+    
+function loginForm () {
+    let name = nameForm.value,
+        password = passwordForm.value;
+
+    if (name == user.name && password == user.password) {
+        h1.innerText = `Welcome ${user.name}`;
+    } else {
+        h2.innerText = `unknown user`;
+    }
+}
+
+registerButton.addEventListener("click", registerForm);
+loginButton.addEventListener("click", loginForm);
